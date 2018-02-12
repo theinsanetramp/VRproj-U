4b7fd3d81d741a52fa2ec820c4a94e15f948e052
theinsanetramp <amelvin15@gmail.com>
2018-02-12 17:44:12 +0000

    Fill threads complete

--- UDPTests/CamSendThreaded/CannyDetector_demo.cpp
+++ UDPTests/CamSendThreaded/CannyDetector_demo.cpp
@@ -22,21 +22,25 @@ using namespace std;
 #define BUFLEN 40960
 
 #define FRAMERATE 10
-#define POINTSPERTHREAD 10
+#define POINTSPERTHREAD 2
 
 Mat tmp_frame, dst_out;
 Mat dst, flood_mask[4], mean_mask[4];
 //Mat sub_dst[4];
 
-thread t;
+thread t[3];
 int finished = 0;
-int imageReady[3] = {0,0,0};
+int imageReady0 = 0;
+int imageReady1 = 0;
+int imageReady2 = 0;
 mutex m;
 condition_variable conVar;
+condition_variable conVar1;
+condition_variable conVar2;
 
 VideoCapture cap;
 //Canny variables
-int lowThreshold = 15;
+int lowThreshold = 20;
 int const max_lowThreshold = 100;
 int thresh_ratio = 3;
 int kernel_size = 3;
@@ -63,9 +67,13 @@ struct sockaddr_in myaddr, remaddr;
 int fd, bufSize, k, slen=sizeof(remaddr);
 //char server[] = "10.42.0.1";
 uchar buf[BUFLEN];
+uchar buf0[1024];
 uchar buf1[1024];
+uchar buf2[1024];
 int bufLength;
+int buf0Length;
 int buf1Length;
+int buf2Length;
 vector<uchar> dstBuf;
 
 void FindColours(int corner, int offset_cols, int offset_rows, int sobolPoints)
@@ -73,12 +81,12 @@ void FindColours(int corner, int offset_cols, int offset_rows, int sobolPoints)
   //Floodfill from quasi-random points
   //sub_dst[corner] = dst.clone();
   int j = 0;
-  for (unsigned long long i = 0; i < sobolPoints; ++i)
+  for (unsigned long long i = sobolPoints*corner; i < sobolPoints*(corner+1); ++i)
   {
-    int x = offset_cols + dst.cols/2 * sobol::sample(i, 0);
-    int y = offset_rows + dst.rows/2 * sobol::sample(i, 1);
-    //cout << corner << "1\n";
+    int x = dst.cols * sobol::sample(i, 0);
+    int y = dst.rows * sobol::sample(i, 1);
     Point seed = Point(x, y);
+    //circle(dst, seed, 5, 255, -1);
     if(dst.at<Vec3b>(seed)[0] == 0)
     {
       flood_mask[corner] = 0;
@@ -92,8 +100,19 @@ void FindColours(int corner, int offset_cols, int offset_rows, int sobolPoints)
         }
       }
       Scalar newVal = alpha*mean(tmp_frame,mean_mask[corner]);
-      
+      for(int i=0;i<3;i++) if(newVal[i] < 1) newVal[i] = 1;
       if(corner == 0) {
+        //cout << seed << "  " << newVal <<  endl;
+        buf0[j*7] = (x >> 8) & 0xFF;
+        buf0[j*7+1] = x & 0xFF;
+        buf0[j*7+2] = (y >> 8) & 0xFF;
+        buf0[j*7+3] = y & 0xFF;
+        buf0[j*7+4] = newVal[0];
+        buf0[j*7+5] = newVal[1];
+        buf0[j*7+6] = newVal[2];
+        j++;
+      }
+      if(corner == 1) {
         //cout << seed << "  " << newVal <<  endl;
         buf1[j*7] = (x >> 8) & 0xFF;
         buf1[j*7+1] = x & 0xFF;
@@ -104,7 +123,19 @@ void FindColours(int corner, int offset_cols, int offset_rows, int sobolPoints)
         buf1[j*7+6] = newVal[2];
         j++;
       }
+      if(corner == 2) {
+        //cout << seed << "  " << newVal <<  endl;
+        buf2[j*7] = (x >> 8) & 0xFF;
+        buf2[j*7+1] = x & 0xFF;
+        buf2[j*7+2] = (y >> 8) & 0xFF;
+        buf2[j*7+3] = y & 0xFF;
+        buf2[j*7+4] = newVal[0];
+        buf2[j*7+5] = newVal[1];
+        buf2[j*7+6] = newVal[2];
+        j++;
+      }
       else if(corner == 3) {
+        //cout << seed << "  " << newVal <<  endl;
         buf[j*7] = (x >> 8) & 0xFF;
         buf[j*7+1] = x & 0xFF;
         buf[j*7+2] = (y >> 8) & 0xFF;
@@ -117,7 +148,14 @@ void FindColours(int corner, int offset_cols, int offset_rows, int sobolPoints)
     }
   }
   if(corner == 0) {
+    buf0Length = j;
+  }
+  if(corner == 1) {
     buf1Length = j;
+  }
+  if(corner == 2) {
+    buf2Length = j;
+    //for(int i=0;i<buf2Length*7+7;i++) cout << (int)buf2[i] << " ";
   }
   else if(corner == 3) {
     bufLength = j;
@@ -131,39 +169,44 @@ void FindColoursThread0(int corner, int sobolPoints)
   while(!finished)
   {
     unique_lock<mutex> lk(m);
-    conVar.wait(lk, []{return imageReady[0];});
-    //cout << "running" << endl;
+    conVar.wait(lk, []{return imageReady0;});
+    //cout << "running" << corner << endl;
     FindColours(corner, offset_cols, offset_rows, sobolPoints);
-    imageReady[0] = 0;
+    imageReady0 = 0;
     lk.unlock();
     conVar.notify_one();
   }
 }
   
-void FindColoursThreadGeneral(int corner, int sobolPoints)
+void FindColoursThread1(int corner, int sobolPoints)
 {
-  int offset_cols;
-  int offset_rows;
-  switch(corner) {
-    case 1:
-      offset_cols = tmp_frame.cols/2;
-      offset_rows = 0;
-      break;
-    case 2:
-      offset_cols = 0;
-      offset_rows = tmp_frame.rows/2;
-      break;
-    default:
-      cout << "FindColours: Unknown corner" << endl;
-  }
+  int offset_cols = tmp_frame.cols/2;
+  int offset_rows = 0;
   while(!finished)
   {
     unique_lock<mutex> lk(m);
-    conVar.wait(lk, []{return imageReady[0];});
-    //cout << "running" << endl;
+    conVar1.wait(lk, []{return imageReady1;});
+    //cout << "running" << corner <<  endl;
     FindColours(corner, offset_cols, offset_rows, sobolPoints);
-    imageReady[corner] = 0;
+    imageReady1 = 0;
     lk.unlock();
+    conVar1.notify_one();
+  }
+}
+
+void FindColoursThread2(int corner, int sobolPoints)
+{
+  int offset_cols = 0;
+  int offset_rows = tmp_frame.rows/2;
+  while(!finished)
+  {
+    unique_lock<mutex> lk(m);
+    conVar2.wait(lk, []{return imageReady2;});
+    //cout << "running" << corner <<  endl;
+    FindColours(corner, offset_cols, offset_rows, sobolPoints);
+    imageReady2 = 0;
+    lk.unlock();
+    conVar2.notify_one();
   }
 }
 
@@ -180,29 +223,53 @@ void CannyThreshold(int, void*)
   //erode( dst, dst, element );
   //cvtColor(dst, dst, CV_GRAY2RGB);
   dst_out = dst.clone(); 
+  //cout << "edges\n";
   {
     lock_guard<mutex> lk(m);
-    for(int i=0;i<3;i++) imageReady[i] = 1;
+    imageReady0 = 1;
   }
   conVar.notify_one();
-  FindColours(3, dst.cols/2, tmp_frame.rows/2, POINTSPERTHREAD);
-  //cout << "0\n";
-  { 
-    unique_lock<mutex> lk(m);
-    conVar.wait(lk, []{return !imageReady[0];});
+  {
+    lock_guard<mutex> lk(m);
+    imageReady1 = 1;
   }
-  while(imageReady[1] == 1 && imageReady[2] == 1);
-  //cout << "1\n";
-  for(int i=0;i<buf1Length*7;i++) buf[bufLength*7 + i] = buf1[i];
-  for(int i=0;i<7;i++) buf[bufLength*7 + buf1Length*7 + i] = 0;
+  conVar1.notify_one();
+  {
+    lock_guard<mutex> lk(m);
+    imageReady2 = 1;
+  }
+  conVar2.notify_one();
+  FindColours(3, dst.cols/2, dst.rows/2, POINTSPERTHREAD);
+  //cout << "running3\n";
+  {
+    unique_lock<mutex> lk(m);
+    conVar.wait(lk, []{return !imageReady0;});
+  }
+  {
+    unique_lock<mutex> lk(m);
+    conVar1.wait(lk, []{return !imageReady1;});
+  }
+  {
+    unique_lock<mutex> lk(m);
+    conVar2.wait(lk, []{return !imageReady2;});
+  }
+  //cout << "sending\n";
+  for(int i=0;i<buf0Length*7;i++) buf[bufLength*7 + i] = buf0[i];
+  for(int i=0;i<buf1Length*7;i++) buf[bufLength*7 + buf0Length*7 + i] = buf1[i];
+  //for(int i=0;i<buf0Length*7+bufLength*7+buf1Length*7+14;i++) cout << (int)buf[i] << " ";
+  for(int i=0;i<buf2Length*7;i++) buf[bufLength*7 + buf0Length*7 + buf1Length*7 + i] = buf2[i];
+  for(int i=0;i<7;i++) buf[bufLength*7 + buf0Length*7 + buf1Length*7 + buf2Length*7 + i] = 0;
   resize(dst_out, dst_out, Size(), 0.5, 0.5, CV_INTER_AREA);
   imencode(".png", dst_out, dstBuf);
   //cout << bufLength*7 << "  " << buf1Length*7 << "  " << dstBuf.size() << endl;
   //cout << "2\n";
-  for(int i=0;i<dstBuf.size();i++) buf[bufLength*7 + buf1Length*7 + 7 + i] = dstBuf[i];
-  //for(int i=0;i<buf1Length*7+bufLength*7+14;i++) cout << (int)buf[i] << endl;
+  for(int i=0;i<dstBuf.size();i++) buf[bufLength*7 + buf0Length*7 + buf1Length*7 + buf2Length*7 + 7 + i] = dstBuf[i];
+  //for(int i=0;i<(buf1Length+bufLength+buf0Length+buf2Length+2);i++) {
+  //   for(int a=0;a<7;a++) cout << (int)buf[i*7+a] << " ";
+  //   cout << endl;
+  //}
   //cout << "done\n";
-  bufSize = bufLength*7 + buf1Length*7 + 7 + dstBuf.size();
+  bufSize = bufLength*7 + buf0Length*7 + buf1Length*7 + buf2Length*7 + 7 + dstBuf.size();
   //imshow( "Edge Map", dst );
  }
 
@@ -263,13 +330,14 @@ int main( int argc, char** argv )
 
   //namedWindow( "Edge Map", 1 );
   //cvMoveWindow( "Edge Map", 0, 40 );
-  //namedWindow("Camera", 1);
+  namedWindow("Camera", 1);
   //cvMoveWindow( "Camera", tmp_frame.cols + 20, 40 );
   //createTrackbar( "Min Threshold:", "Edge Map", &lowThreshold, max_lowThreshold, CannyThreshold );
   //createTrackbar( "Blur Kernel Size:", "Edge Map", &blur_kernel, max_blur_kernel, 0 );
 
-  //for(int i=0;i<1;i++) t[i] = thread(FindColoursThread, i, 20);
-  t = thread(FindColoursThread, 0, POINTSPERTHREAD);
+  t[0] = thread(FindColoursThread0, 0, POINTSPERTHREAD);
+  t[1] = thread(FindColoursThread1, 1, POINTSPERTHREAD);
+  t[2] = thread(FindColoursThread2, 2, POINTSPERTHREAD);
 
   for(;;)
   {
@@ -285,7 +353,7 @@ int main( int argc, char** argv )
 
     /// Show the image
     CannyThreshold(0, 0);
-    //imshow("Camera", tmp_frame);
+    imshow("Camera", tmp_frame);
 
     if (sendto(fd, buf, bufSize, 0, (struct sockaddr *)&remaddr, slen)==-1)
       perror("sendto");
@@ -297,7 +365,7 @@ int main( int argc, char** argv )
         }
   }
   finished = 1;
-  //for(int i=0;i<3;i++) t[i].join();
-  t.join();
+  for(int i=0;i<3;i++) t[i].join();
+  //t.join();
   return 0;
   }
