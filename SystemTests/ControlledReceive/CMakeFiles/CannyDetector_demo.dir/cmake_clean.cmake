4634d1643f8d3f15ce14f745bea12ce0d7eb05db
theinsanetramp <amelvin15@gmail.com>
2018-04-12 16:54:31 +0100

    BNO testing

---
+++ SystemTests/ControlledReceive/CMakeFiles/CannyDetector_demo.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CannyDetector_demo.dir/CannyDetector_demo.cpp.o"
+  "CMakeFiles/CannyDetector_demo.dir/sobol.cpp.o"
+  "CMakeFiles/CannyDetector_demo.dir/gamepad.c.o"
+  "CannyDetector_demo.pdb"
+  "CannyDetector_demo"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/CannyDetector_demo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
